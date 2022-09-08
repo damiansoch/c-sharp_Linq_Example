0a10c81f34d70c641d89293f016f1b89f0f3e53b
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-09-08 09:43:28 +0100

    Add project files.

---
+++ Linq_Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //specify data source
+            string[] students = new string[] { "Adam", "Joshua", "Adrian", "Gregory", "Anna", "Adrian", "Derek", "Damian", "Simon", "Eustachy", "Alex", };
+
+            //define the query expression
+            IEnumerable<string> Names = 
+                from student in students
+                where student.Length>=6
+                select student;
+
+            //Execute the query
+            foreach (string name in Names)
+            {
+                Console.Write(name + " ");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
